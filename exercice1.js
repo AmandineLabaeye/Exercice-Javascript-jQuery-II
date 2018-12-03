6bb0a5f40c9bb1bc18e60ef5d21d9b26022d8b3d
Amandine <amandine.labaeye@orange.fr>
2018-12-03 10:42:28 +0100

    commit

---
+++ exercice1.js
@@ -0,0 +1,9 @@
+// Remplace tout le text des "li" par horn
+$("li").text("horn");
+
+// Remplace le titre principal grâce à son id
+$("#page-heading").text("All about unicorns");
+
+// Remplace le text des elements html comportant
+// la class "animal"
+$(".animal").text("unicorns");
